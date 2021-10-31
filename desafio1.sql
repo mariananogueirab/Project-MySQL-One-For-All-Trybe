0d7abf5aa6e804e968f8543d6a82833853826d15
mariananbp <mariananbp@gmail.com>
2021-10-30 23:47:31 -0300

    Changing valor_plano type

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ USE SpotifyClone;
 CREATE TABLE planos (
 	plano_id INT PRIMARY KEY AUTO_INCREMENT,
     plano VARCHAR(50) NOT NULL,
-    valor_plano DOUBLE NOT NULL
+    valor_plano DECIMAL(5, 2) NOT NULL
 );
 
 INSERT INTO planos (plano, valor_plano)
