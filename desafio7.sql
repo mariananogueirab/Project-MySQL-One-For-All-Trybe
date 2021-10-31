86bb375f829a56a0b176f1f72d3b7a74ed242132
mariananbp <mariananbp@gmail.com>
2021-10-30 23:43:38 -0300

    Challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS SELECT ar.artista AS `artista`,
+al.album AS `album`,
+COUNT(s.artista_id) AS `seguidores`
+FROM SpotifyClone.albuns AS al
+INNER JOIN SpotifyClone.artistas AS ar
+ON ar.artista_id = al.artista_id
+INNER JOIN SpotifyClone.seguindo AS s
+ON ar.artista_id = s.artista_id
+GROUP BY al.album_id;
